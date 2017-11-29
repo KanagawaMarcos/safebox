95aae262ec1fc49eebe7c1fa4711d59d23484519
Marcos Costa <mcs1901@outlook.com>
2017-11-29 09:13:56 -0300

    adiciona o arquivo de conexão com o Banco de Dados

---
+++ includes/petdb.php
@@ -0,0 +1,9 @@
+<?php
+  $dbServername = "localhost";
+  $dbUsername = "root";
+  $dbPassword = "loginsystem";
+  $dbname = "historico";
+
+  $connection = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbname);
+  
+?>
