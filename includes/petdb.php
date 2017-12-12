ebf75e4231c3a05c941350a479f188669a8b69d7
Marcos Costa <mcs1901@outlook.com>
2017-12-12 09:15:46 -0300

    Commit inicial do MVP

--- includes/petdb.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $dbServername = "localhost";
-  $dbUsername = "root";
-  $dbPassword = "loginsystem";
-  $dbname = "historico";
-
-  $connection = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbname);
-  
-?>
