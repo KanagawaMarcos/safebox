22777487bdcf177cad8f510d450f5ae8f2eae8b3
Marcos Costa <mcs1901@outlook.com>
2017-12-18 15:31:20 -0300

    Prepared statement adicionado à verificação se o valor do saque é válido

--- includes/saque_inc.php
+++ includes/saque_inc.php
@@ -18,37 +18,51 @@ if(isset($_POST['submit']) AND isset($_SESSION['u_id'])){
         exit();
     }
     //olha se é possível realizar o saque
-    $sql = "SELECT * FROM caixinhas WHERE caixinha_id='$saqueid'";
-    $result = mysqli_query($conn, $sql);
-    $row = mysqli_fetch_assoc($result);
-    if($row['caixinha_value'] < $valor){
-        header("Location: ../home.php?index=tooHigh");
-        exit();
-    }
-    if(empty($agente) || empty($valor)){
-        header("Location: ../home.php?index=empty");
-        exit();
-    } else {
-        if (!preg_match("/^[a-zA-Z]*$/", $agente) || !is_numeric($valor) || $valor<=0) {
-            header("Location: ../home.php?index=invalidInput");
-            exit();
-        } else {
-            $sqlSearch = "SELECT * FROM users WHERE user_uid='$agente'";
-            $searchResult = mysqli_query($conn, $sqlSearch);
-            $resultCheck = mysqli_num_rows($searchResult);
-            if($resultCheck > 0){
-                //o usuário existe
-                $sqlDepositar = "UPDATE caixinhas SET caixinha_value=(caixinha_value - '$valor') WHERE caixinha_id = '$saqueid'";
-                mysqli_query($conn, $sqlDepositar);
-                $sqlInserirSaque = "INSERT INTO varys(tipo, valor, agente, origem, destino) VALUES ('$tipo','$valor','$agente','$origem','$destino')";
-                mysqli_query($conn, $sqlInserirSaque);
-                header("Location: ../home.php?saque=sucess");
-                exit();
-            } else {
-                header("Location: ../home.php?index=error");
-                exit();
-            }
-        }
+    //$sql = "SELECT * FROM caixinhas WHERE caixinha_id='$saqueid'";
+    $statement = mysqli_stmt_init($conn);
+    $sql = "SELECT * FROM caixinhas WHERE `caixinha_id`=?";
+
+    if(!mysqli_stmt_prepare($statement, $sql)){
+      header("Location: ../home.php?stmt=errorBind");
+      mysqli_stmt_close($statement);
+      exit();
+    }else{
+      mysqli_stmt_bind_param($statement, "i", $saqueid);
+      mysqli_stmt_execute($statement);
+
+      //$result = mysqli_query($conn, $sql);
+      $result = mysqli_stmt_get_result($statement);
+      $row = mysqli_fetch_assoc($result);
+
+      if($row['caixinha_value'] < $valor){
+          header("Location: ../home.php?index=tooHigh");
+          exit();
+      }
+      if(empty($agente) || empty($valor)){
+          header("Location: ../home.php?index=empty");
+          exit();
+      } else {
+          if (!preg_match("/^[a-zA-Z]*$/", $agente) || !is_numeric($valor) || $valor<=0) {
+              header("Location: ../home.php?index=invalidInput");
+              exit();
+          } else {
+              $sqlSearch = "SELECT * FROM users WHERE user_uid='$agente'";
+              $searchResult = mysqli_query($conn, $sqlSearch);
+              $resultCheck = mysqli_num_rows($searchResult);
+              if($resultCheck > 0){
+                  //o usuário existe
+                  $sqlDepositar = "UPDATE caixinhas SET caixinha_value=(caixinha_value - '$valor') WHERE caixinha_id = '$saqueid'";
+                  mysqli_query($conn, $sqlDepositar);
+                  $sqlInserirSaque = "INSERT INTO varys(tipo, valor, agente, origem, destino) VALUES ('$tipo','$valor','$agente','$origem','$destino')";
+                  mysqli_query($conn, $sqlInserirSaque);
+                  header("Location: ../home.php?saque=sucess");
+                  exit();
+              } else {
+                  header("Location: ../home.php?index=error");
+                  exit();
+              }
+          }
+      }
     }
 } else {
     header("Location: ../index.php");
