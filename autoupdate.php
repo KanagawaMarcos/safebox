024bf82d63d8dd6e98af8fa57322b895b0a08671
Marcos Costa Santos <mcs1901@outlook.com>
2018-01-08 22:57:30 -0300

    autoupdate.php remake

--- autoupdate.php
+++ autoupdate.php
@@ -1,12 +1,6 @@
 <?php
-	$deleteOlderBackup = shell_exec('rm -r ~/backup/box');
-	$updateOlderBackup = shell_exec('mv ~/box ~/backup ');
-
-	$createNewBackup = shell_exec('cp -r /var/www/box ~/');
-	$gitPull = shell_exec('git pull');
-
-	$to = 'mcs1901@outlook.com';
-	$subject = 'Auto update report from PetBox';
-	$message = 'delete older backup: '.$deleteOlderBackup.'// update older backup: '.$updateOlderBackup.' // create new backup: '.$createNewBackup.'  // git pull: '.$gitPull;
-	mail($to,$subject,$message)
+`rm -r ~/backup/box)`;
+`mv ~/box ~/backup )`;
+`cp -r /var/www/box ~/`;
+`git pull`;
  ?>
