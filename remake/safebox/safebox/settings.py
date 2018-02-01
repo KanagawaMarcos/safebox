2e7cd69f2f447b554fa5550365381572f45b9692
Marcos Costa <mcs1901@outlook.com>
2018-02-01 10:54:01 -0300

    interface de login criada (back e front)

--- remake/safebox/safebox/settings.py
+++ remake/safebox/safebox/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'safebox.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    #'/var/www/static/',
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_safebox_cdn")
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_safebox_cdn")
