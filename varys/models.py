8d5fda6568499f6367ead7101f89e20a52c8774a
Marcos Costa <mcs1901@outlook.com>
2018-02-14 13:35:23 -0300

    Criado o modelo de transação mas apresenta problemas na migração

--- varys/models.py
+++ varys/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Transaction(models.Model):
+	value = models.DecimalField(max_digits=6, decimal_places=2)
+	justification = models.CharField(max_length=257)
+	#date = models.DateField(_(Date), default=datetime.date.today)
+	#date = models.DateField(_("Date"), auto_now_add=True)
+	user = models.ForeignKey(User)
+
+	def __str__(self):
+		return self.justification + " ( R$ " + str(value) +" ) ( " + str(date)+" )"
