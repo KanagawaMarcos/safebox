d4d093326b5543cbee1bb44e91938c0762993771
Marcos Costa <mcs1901@outlook.com>
2018-02-17 14:42:31 -0300

    python file to import mysql table of varys (old version) completed

---
+++ include_varys.py
@@ -0,0 +1,32 @@
+import csv
+
+def carregar():
+    with open("varys.csv") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            tipo = ''
+            origem=''
+            destino=''
+            if row[1] == 'deposito':
+                tipo = 'Deposito'
+                origem = ''
+                destino = 'Caixa Geral'
+            else:
+                tipo = 'Saque'
+                origem = 'Caixa Geral'
+                destino = ''
+            quem = ''
+            if row[3] == 'arthurcst':
+                quem = 'Arthur Costa'
+            else:
+                quem = 'Geraldo Braz'
+            created = Transaction()
+            created.who_did_it = quem
+            created.value = row[2]
+            created.justification = row[7]
+            created.created_date = row[5]
+            created.origin = origem
+            created.destination = destino
+            created.its_type = tipo
+            created.save()
+            print(created)
