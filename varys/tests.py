715aa0abf01fda7f6612ef6fa8e0acc173b2f8e4
Marcos Costa <mcs1901@outlook.com>
2018-02-19 16:44:42 -0300

    csv do antigo bando de dados movido

--- varys/tests.py
+++ varys/tests.py
@@ -1,16 +1,20 @@
 from django.test import TestCase
 from varys.models import Transaction
 
+from random
 # Create your tests here.
 class Transaction_Model_Test(TestCase):
 
     def setUp(self):
         deposit = Transaction.objects.create(
-            who_did_it='Positive_Deposit',
-            value=10,
-            justification='Testing positive values',
+            who_did_it='Generic_Deposit',
+            value=0,
+            justification='Testing some deposits',
             its_type='Deposito',
         )
-    def test_positive_deposits(self):
-        positive_deposit = Transaction.objects.get(who_did_it='Positive_Deposit')
+    def test_deposits(self):
+
+        #Test if only values greater or equal to 0 are valid
+        positive_deposit = Transaction.objects.get(who_did_it='Generic_Deposit')
+        positive_deposit.value = random.uniform(0.05 , 16000.0)
         self.assertEqual(positive_deposit.value > 0.0 , True)
