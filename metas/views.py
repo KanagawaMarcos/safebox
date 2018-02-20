a3ffc635c0f219b712df74b30aaaaa456f56c316
Marcos Costa <mcs1901@outlook.com>
2018-02-20 13:11:57 -0300

    Template metas adicionado

--- metas/views.py
+++ metas/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# Create your views her
+@login_required
+def metas(request):
+    return render(request, 'metas/metas.html', {})
