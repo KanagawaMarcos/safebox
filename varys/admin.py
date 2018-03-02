64bddabf1133f11a1312eadab9af5a60efa93b2b
Marcos Costa <mcs1901@outlook.com>
2018-03-01 23:07:32 -0300

    Corrige problemas com modelo da caixa

--- varys/admin.py
+++ varys/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Transaction
+from .models import Box
 
 # Register your models here.
 admin.site.register(Transaction)
+admin.site.register(Box)
--- varys/models.py
+++ varys/models.py
@@ -25,5 +25,7 @@ class Box (models.Model):
 	name = models.CharField(max_length=257)
 	value = models.DecimalField(max_digits=6, decimal_places=2)
 
+	class Meta:
+		verbose_name_plural = "boxes"
 	def __str__(self):
 		return "%s - R$ %s" %(self.name, self.value)
