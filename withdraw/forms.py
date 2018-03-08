aed6685cd8c422d93f6b9cdcf77eb45734f1ae77
Marcos Costa <mcs1901@outlook.com>
2018-03-08 18:02:35 -0300

    clean the WithdrawForm class

--- withdraw/forms.py
+++ withdraw/forms.py
@@ -5,16 +5,8 @@ class WithdrawForm(forms.Form):
 
     value = forms.DecimalField()
     justification = forms.CharField(widget=forms.Textarea(attrs={'class':'materialize-textarea'}))
-    receipt = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+    #receipt = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
 
     class Meta:
         model = Transaction
-        fields = ('value','justification','receipt')
-
-    # def clean_value(self):
-    #     self.cleaned_data.get('value')
-    #     # validators=[
-    #     #         MaxValueValidator(16000,"Valor Alto demais."),
-    #     #         MinValueValidator(0.05, "Valor Baixo demais."),
-    #     #         DecimalValidator(5,2,3)
-    #     #     ]
+        fields = ('value','justification')
