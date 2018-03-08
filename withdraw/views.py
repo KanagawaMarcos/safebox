70a116bcd62fac637f3108823dd16e4e52ce5d77
Marcos Costa <mcs1901@outlook.com>
2018-03-08 18:23:48 -0300

    Withdraw form working

--- withdraw/views.py
+++ withdraw/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 
 # from varys.models import Transaction
 from .forms import WithdrawForm
@@ -16,22 +17,24 @@ def saque(request):
     #Se esse view está recebendo um POST, ou seja
     #já foi renderizado e o formulário completado
     if request.method == 'POST':
-        #Copia o objeto de transação enviados via método POST
-        transaction = Transaction(request.POST)
+        #Copia os dados de transação enviados via método POST
+        #E cria um formulário novo com eles
+        form = WithdrawForm(request.POST)
 
         #Se os dados foram preenchido corretamente
-        if transaction.is_valid():
+        if form.is_valid():
             return HttpResponseRedirect('/thanks/')
         else:
-            return HttpResponseRedirect('/deu-ruim')
+            return HttpResponseRedirect('/deu-ruim/')
     #Se for a primeira vez que a página é renderizada
     else:
         #Cria uma transação vazia para ser preenchida
-        transaction = WithdrawForm()
+        form = WithdrawForm()
 
-    return render(request, 'shell/app_shell.html',
-                    {'is_withdraw': True,
+    return render(request, 'shell/app_shell.html',{
+                     'is_withdraw': True,
                      'title': 'Saque',
-                     'transaction': transaction,
+                     'transaction': form,
                      'boxes': Box.objects.all(),
-                     'users_list': User.objects.all()})
+                     'users_list': User.objects.all()
+                     })
