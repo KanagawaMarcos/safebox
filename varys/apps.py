8dd4b705ca5aefdb5d28ab7e2a545c7585b6e868
Marcos Costa <mcs1901@outlook.com>
2018-03-15 17:02:12 -0300

    Deposit and Varys apps have now a verbose name in PT-BR

--- varys/apps.py
+++ varys/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class VarysConfig(AppConfig):
     name = 'varys'
+    verbose_name = 'Histórico'
--- withdraw/apps.py
+++ withdraw/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class WithdrawConfig(AppConfig):
     name = 'withdraw'
+    verbose_name = 'Saques'
