b9c3be27330c5d3fa86ca621ef5ff93c0cc7e322
ufma <petcompufma2015@gmail.com>
2018-03-27 12:34:21 -0300

    edit files to support deployment in the server

--- safebox/settings/base.py
+++ safebox/settings/base.py
@@ -82,7 +82,8 @@ WSGI_APPLICATION = 'safebox.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	 'NAME': 'db.sqlite3',
     }
 }
 
