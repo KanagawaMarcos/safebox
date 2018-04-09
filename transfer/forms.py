e59a744d268869c60430ab6dc3f9846ae38bf495
Marcos Costa <mcs1901@outlook.com>
2018-04-09 13:23:19 -0300

    Bug fix: Transference form validations invalid logic

--- transfer/forms.py
+++ transfer/forms.py
@@ -8,6 +8,10 @@ from varys.forms import MultipleTransactionForm,BasicInfoForm
 # Get the base form for each form
 from transfer.models import Transference
 
+# Allows to check if a safebox has money to do a transfer
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
 class TransferenceForm(BasicInfoForm):
 
     origin = forms.ModelChoiceField(Box.objects.all())
@@ -16,7 +20,7 @@ class TransferenceForm(BasicInfoForm):
     def clean(self):
 
         # Don't allow a transference if there isn't money available.
-        if self.cleaned_data['value'] <= self.cleaned_data['origin'].value :
+        if self.cleaned_data['value'] > self.cleaned_data['origin'].value :
             raise ValidationError(_('A caixinha não tem esta quantia em dinheiro.'))
 
     class Meta:
