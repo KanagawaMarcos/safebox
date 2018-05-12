85cde80b1cc8c2c9f2c64a3487bea6b7fa4275c3
Marcos Costa <mcs1901@outlook.com>
2018-05-12 18:11:51 -0300

    update to try fix comprovantes url problem

--- safebox/urls.py
+++ safebox/urls.py
@@ -48,7 +48,8 @@ urlpatterns = [
     path('transferencia/', transfer.views.transferencia, name='transferencia'),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
