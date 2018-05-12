f38745947848ac77909d0e72fc36a0ee8c1c1755
Marcos Costa <mcs1901@outlook.com>
2018-05-12 18:15:51 -0300

    fix bug of receipt url broken

--- safebox/urls.py
+++ safebox/urls.py
@@ -48,6 +48,7 @@ urlpatterns = [
     path('transferencia/', transfer.views.transferencia, name='transferencia'),
 ]
 
+if settings.DEBUG:
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
